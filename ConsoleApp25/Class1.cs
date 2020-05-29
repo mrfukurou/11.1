a7f36508d20c1d6d8e704b1b880fe02186b88479
fukur <fukur@LAPTOP-7EQMGM6H>
2020-05-29 23:23:00 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp25/Class1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp25
+{
+    class mas
+    {
+
+        int[,] IntArray;
+        int n;
+
+        public mas(int N)
+        {
+            n = N;
+            IntArray = new int[n, n];
+        }
+
+        public int[,] Input()
+        {
+            Console.WriteLine("Введите элементы массива: ");
+            for (int i = 0; i < n; ++i)
+            {
+                for (int j = 0; j < n; ++j)
+                {
+                    IntArray[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+            return IntArray;
+
+        }
+
+        public void Output()
+
+        {
+            for (int i = 0; i < IntArray.GetLength(0); ++i, Console.WriteLine())
+                for (int j = 0; j < IntArray.GetLength(1); ++j)
+                    Console.Write("{0,5} ", IntArray[i, j]);
+
+        }
+
+        public void summ(int m)
+        {
+           
+            int sum = 0;
+            for (int i = 0; i < n; i++)
+            {
+                sum += IntArray[i, m];
+                
+            }
+            Console.WriteLine("Сумма столбцa: " + sum);
+        }
+
+
+        public int yong
+        {
+            get
+            {
+                int zero = 0;
+                for (int i = 0; i < IntArray.GetLength(0); i++)
+                {
+                    for (int j = 0; j < IntArray.GetLength(1); j++)
+                    {
+                        if (IntArray[i, j] == 0)
+                            zero++;
+                    }
+                }
+                return zero;
+            }
+        }
+
+        public int Scalar
+        {
+            set
+            {
+                for (int i = 0; i < IntArray.GetLength(0); i++)
+                {
+                    IntArray[i, i] = value;
+                }
+            }
+        }
+    }
+}
+
+
---
+++ ConsoleApp25/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleApp25
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Введите размер массива: ");
+                int n = Convert.ToInt32(Console.ReadLine());
+                mas ms = new mas(n);
+                int[,] IntArray = ms.Input();
+                Console.WriteLine("Матрица: ");
+                ms.Output();
+                Console.Write("Введите номер столбца:");
+                int b = Convert.ToInt32(Console.ReadLine());
+                ms.summ(b);
+                Console.WriteLine("Количество нулевых элементов: " + ms.yong);
+                Console.Write("Введите скаляр: ");
+                int m = Convert.ToInt32(Console.ReadLine());
+                ms.Scalar = m;
+                ms.Output();
+            }
+            catch { Console.Write("Некорректный ввод!"); }
+        }
+    }
+}
